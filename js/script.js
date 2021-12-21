b68a55d79702b1cdbdcfafd158c7b6f51288eaf4
JuliaMuk <den_suvorov2003@mail.ru>
2021-12-22 01:48:06 +0500

    main

--- js/script.js
+++ js/script.js
@@ -1,46 +1,133 @@
-const elems = document.querySelectorAll(".menu_item a");
-for (let elem of elems) {
-    elem.setAttribute("href", "https://midis.ru/")
-}   
+const d = document;
+const cl = console.log;
 
-const bl3 = document.getElementById("block3");
-bl3.innerHTML = "новый <b>блок</b>";
+let lists = JSON.parse(localStorage.lists || "[]"); 
+let card_num = JSON.parse(localStorage.card_num || 1);
 
-bl3.setAttribute("class", "bg-red");
-bl3.classList.add("big-text");
-bl3.classList.remove("big-text");
-console.log(bl3.classList.contains("bg-red"));
+const btnCreateList = d.getElementById("btn-create-list");
+const desk = d.getElementById("desk");
+const btnRemoveDesk = d.getElementById("btn-remove-desk");
+const inputListName = d.getElementById("list-name");
 
-bl3.addEventListener("click",function (){
-    bl3.classList.toggle("bg-red");
+for (let i in lists) {
+    outputList(lists[i])        
+}
+
+function setAtrs(el, attrs) {
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key])
+    }
+}
+
+function addList() {
+    let listName = d.getElementById("list-name").value;
+    if (listName == '') {
+        listName = "Новый список " + card_num;
+    }
+    card_num++;
+    localStorage.card_num = JSON.stringify(card_num);
+
+
+    outputList(listName);
+    lists.push(listName);
+    localStorage.lists = JSON.stringify(lists);
+}
+
+// function changeList(list) {
+//     let currentList = lists.indexOf(listName);
+//     lists[currentList] = list;
+//     localStorage.lists = JSON.stringify(lists);
+// }
+
+function outputList(listName) {
+    let list = d.createElement("div");
+    let h2 = d.createElement("h2");
+    let div = d.createElement("div");
+    div.classList.add("list-header");
+    let img = d.createElement("img");
+    setAtrs(img, { "src": "img/edit-solid.svg", "alt": "Редактировать название списка", "width": "20px"});
+    let list2 = d.createElement("div");     // лист
+    list2.classList.add("list-cards");
+    let p = d.createElement("p");
+    p.textContent="+ Добавить карточку";
+    p.classList.add("add-card");
+    let span = d.createElement("span");
+    setAtrs(span, {"id":"delete-list"});
+    img.classList.add("edit-list");
+    
+    h2.innerHTML = listName;
+
+    span.innerHTML="✖";
+    list.append(div);
+    div.append(h2);
+    div.append(img);
+    div.append(span);
+    h2.after(img);
+    list.append(list2);
+    list2.append(p);
+    list.classList.add("list");
+    desk.append(list);
+
+    localStorage.lists = JSON.stringify(lists);
+}
+
+btnCreateList.addEventListener("click", addList);
+
+btnRemoveDesk.addEventListener("click", function () {
+    desk.innerHTML = "";
+    card_num = 1
+    lists = [];
+    localStorage.card_num = JSON.stringify(card_num);
+    localStorage.lists = JSON.stringify(lists);
+})
+
+// =============== Сегодня ================
+
+inputListName.addEventListener("keydown", e => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        btnCreateList.click();
+    }
 });
 
-bl3.style.color = "green";
-bl3.style.fontStyle = "italic";
+function editList(e) {
+    let obj = e.target;
+    if (obj.classList.contains("edit-list")) {
+        let list = obj.closest(".list")
+        let h2 = list.querySelector("h2")
+        h2.setAttribute("contenteditable", "true")
+        h2.focus();
+    }
+    if (obj.id=="delete-list") {
+        let list = obj.closest(".list");
+        list.remove();
 
+        let listName = list.closest("div").querySelector("h2").textContent;
+        let currentList = lists.indexOf(listName);
+        lists.splice(currentList, 1);
+    }
+    if(obj.classList.contains("add-card"))
+    {
+        let list = obj.closest(".list-cards");
+        let div = d.createElement("div");
+        let textarea = d.createElement("textarea");
+        let span = d.createElement("span");
 
-/*------создание нового элемента--------*/
-let newElem = document.createElement("h1");
-newElem.textContent = "Заголовок 1";
-let body = document.querySelector("body");
-//body.prepend(newElem);
+        div.classList.add("card");
+        textarea.classList.add("card-text");
+        span.innerHTML="✖";
+        span.classList.add("delete-text")
 
-let bl2 = document.getElementById("block2");
-bl2.before(newElem);
+        div.append(textarea);
+        div.append(span);
+        list.append(div);
+        div.after(obj);
+    }
+    if(obj.classList.contains("delete-text"))
+    {
+        obj.closest(".card").remove();
+    }
+    localStorage.lists = JSON.stringify(lists);
+}
 
-/*---------Удаление элементов------------- */
-
-let bl1 = document.getElementById("block1");
-bl1.remove();
-
-/*------клонирование элементов---------- */
-
-let newBlock = bl2.cloneNode(true);
-body.append(newBlock);
-
-/*-----предки и потомки------- */
-let lorem = document.querySelector(".lorem");
-let pred = lorem.closest("p");
-pred.classList.add("bg-red");
-
-console.log(bl2.querySelectorAll("li"));
+desk.addEventListener("click", editList);
